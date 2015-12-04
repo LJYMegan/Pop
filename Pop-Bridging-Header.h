56bd986e281a5b8e5f0e6ed236626593c2c859ee
AMartiniouk <oleksa.martiniouk@yale.edu>
2015-12-04 15:58:40 -0500

    arda

---
+++ Pop-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Pop-Bridging-Header.h
+//  Pop
+//
+//  Created by Alex Martiniouk on 12/4/15.
+//  Copyright © 2015 Megan Lim. All rights reserved.
+//
+
+#ifndef Pop_Bridging_Header_h
+#define Pop_Bridging_Header_h
+
+#import <Parse/Parse.h>
+
+#endif /* Pop_Bridging_Header_h */
--- Pop/Pop-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Pop-Bridging-Header.h
-//  Pop
-//
-//  Created by Alex Martiniouk on 12/3/15.
-//  Copyright © 2015 Megan Lim. All rights reserved.
-//
-
-#import <Parse/Parse.h>
